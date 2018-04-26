be58d97bf3258ea8315254a6b8f753dde4e3d4fc
GjohnX <gjohnx88@gmail.com>
2018-04-26 19:51:16 -0400

    login.php

--- login.php
+++ login.php
@@ -2,7 +2,6 @@
 require("support.php");
 
 $topPart = <<<EOBODY
-<div>
 <form action="{$_SERVER['PHP_SELF']}" method="post" class = "form-horizontal">
 <ul class="nav nav-tabs">
 	<li><a href="index.html"><span class="glyphicon glyphicon-home"></span></a></li>
@@ -14,11 +13,8 @@ $topPart = <<<EOBODY
 <input type="text" name="email"  required/></br></br>
 <strong>Password: </strong>
 <input type="password" name="password" required/></br></br>
-
 <input type="submit" name="submitInfoButton" value = "Login"/></br>
 </form>
-</div>
-</div>
 EOBODY;
 
 $bottomPart = "";
@@ -35,27 +31,33 @@ if ($result) {
         $bottomPart .= "<h2>Please Register</h2>";
     	$bottomPart .="<a href=\"register.php\"><button>Register</button></a>";
         $bottomPart .="<a href=\"index.html\"><button>Return to main menu</button></a>";
+        $bottomPart .= "</div>";
+
     }
     else{
         $recordArray = mysqli_fetch_array($result, MYSQLI_ASSOC);
         if (!password_verify($password, $recordArray['password'])) {
 			$bottomPart .= "<strong><h1>Invalid login information provided.</strong><h1><br />";
+            $bottomPart .= "</div>";
 		}
 		else {
 			session_start();
 			$_SESSION['user'] = $login;
-			header("location:edit.php");
+            $_SESSION['email'] = $login;
+
+			header("location:userinterface.php");
 		}
 	}
 }else{
-    $bottomPart .= "<h2>Please Register</h2>";
+    $bottomPart .= "<h2>Cannot Connect to Database Please Contact Us in Contact</h2>";
     $bottomPart .="<a href=\"register.php\"><button>Register</button></a>";
     $bottomPart .="<a href=\"index.html\"><button>Return to main menu</button></a>";
+    $bottomPart .= "</div>";
 }
 
 
 }
 $body = $topPart.$bottomPart;
-$page = generatePage($body);
+$page = generatePage($body, "Login");
 echo $page;
 ?>
