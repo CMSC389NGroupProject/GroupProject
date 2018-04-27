c8c9540b5d7ff1e21eda151ae87be66fb96d10d9
linchen <linchen00314@gmail.com>
2018-04-26 20:03:37 -0400

    same as above

--- edit.php
+++ edit.php
@@ -5,7 +5,7 @@ session_start();
 
 if ($_SESSION['user'] != null) {
     $db_connection = connectToDB();
-
+    
     $query = "SELECT * FROM users WHERE email='{$_SESSION['user']}'";
     $result = $db_connection->query($query);
     if (!$result) {
@@ -42,22 +42,18 @@ if ($_SESSION['user'] != null) {
 	
 	/* Closing connection */
     $db_connection->close();
-<<<<<<< HEAD
-} else if (isset($_POST['Update'])) {
-=======
-
 } else {
     header ("Location: index.html");
 }
 
 if (isset($_POST['Update'])) {
->>>>>>> cf11b478746dd5237c889d4244fef87f0da5e40a
+    
     $emailValue = trim($_POST['email']);
     $nameValue = trim($_POST['name']);
     $telValue = trim($_POST['phone_validation']);
     $gender = $_POST['gender'];
     $newPassword = password_hash($_POST['password'], PASSWORD_BCRYPT);
-
+    
     /* Connecting to the database */
     $db_connection = connectToDB();
     
@@ -74,74 +70,74 @@ if (isset($_POST['Update'])) {
     } else {
         $warning = "<p style='color: red'> Fail to update this user. Please try again with correct information</p>";
     }
-
+    
     $stmt->close();
     $db_connection->close();
-
+    
     $message = "The entry has been updated in the database and the new values are:";
-
+    
 }
 
 $body = <<<EOBODY
-    <script>
-        var check = function() {
-            if (document.getElementById('password').value == document.getElementById('verifyPassword').value) {
-                document.getElementById('message').style.color = 'green';
-                document.getElementById('message').innerHTML = 'matching';
-                document.getElementById('form').setAttribute("action", "{$_SERVER["PHP_SELF"]}");
-            } else {
-                document.getElementById('message').style.color = 'red';
-                document.getElementById('message').innerHTML = 'not matching';
-                document.getElementById('form').removeAttribute("action");
-            }
-        }
-    </script>
+<script>
+var check = function() {
+    if (document.getElementById('password').value == document.getElementById('verifyPassword').value) {
+        document.getElementById('message').style.color = 'green';
+        document.getElementById('message').innerHTML = 'matching';
+        document.getElementById('form').setAttribute("action", "{$_SERVER["PHP_SELF"]}");
+    } else {
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerHTML = 'not matching';
+        document.getElementById('form').removeAttribute("action");
+    }
+}
+</script>
 
-    <form action="{$_SERVER["PHP_SELF"]}" method="post" id="form">
-        <div class="form-group">
-            <label style="display: block;">
-                <b>Name: </b> 
-                <input type="text" name="name" value="$name" required><br>
-            </label>
+<form action="{$_SERVER["PHP_SELF"]}" method="post" id="form">
+<div class="form-group">
+<label style="display: block;">
+<b>Name: </b> 
+<input type="text" name="name" value="$name" required><br>
+</label>
 
-            <label style="display: block;">
-                <b>Email: </b> 
-                <input type="email" name="email" value="$email" required><br>
-            </label>
+<label style="display: block;">
+<b>Email: </b> 
+<input type="email" name="email" value="$email" required><br>
+</label>
 
-            <label style="display: block;">
-                <b>Email: </b> 
-                <input type="text" name="phone_validation" pattern="\([0-9]{3}\)[0-9]{3}[\-][0-9]{4}$" required
-               title="Please enter in form: (123)456-7890" class="form-control" value="$tel">
-            </label>
+<label style="display: block;">
+<b>Email: </b> 
+<input type="text" name="phone_validation" pattern="\([0-9]{3}\)[0-9]{3}[\-][0-9]{4}$" required
+title="Please enter in form: (123)456-7890" class="form-control" value="$tel">
+</label>
 
-            <label style="display: block;">
-                <b>Gender: </b> <br>
-                M <input type="radio" name="gender" value="M" $checkedMale>
-                F <input type="radio" name="gender" value="F" $checkedFemale>
-            </label>
+<label style="display: block;">
+<b>Gender: </b> <br>
+M <input type="radio" name="gender" value="M" $checkedMale>
+F <input type="radio" name="gender" value="F" $checkedFemale>
+</label>
 
-            <label style="display: block;">
-                <b>New Password: </b>
-                <input type="password" name="password" id="password" onkeyup='check();' required>
-            </label>
+<label style="display: block;">
+<b>New Password: </b>
+<input type="password" name="password" id="password" onkeyup='check();' required>
+</label>
 
-            <label style="display: block;">
-                <b>Verify Password: </b>
-                <input type="password" name="verifyPassword" id="verifyPassword" onkeyup='check();' required>
-            </label>
-            <span id='message'></span>
-        </div>
+<label style="display: block;">
+<b>Verify Password: </b>
+<input type="password" name="verifyPassword" id="verifyPassword" onkeyup='check();' required>
+</label>
+<span id='message'></span>
+</div>
 
-        <button type="submit" name="Update">Submit Change</button>
-    </form>
+<button type="submit" name="Update">Submit Change</button>
+</form>
 EOBODY;
 
 $updated = <<<EOBODY
-    <h2>$message</h2>
-    <form action="{$_SERVER["PHP_SELF"]}" method="post">
-        $content
-    </form>
+<h2>$message</h2>
+<form action="{$_SERVER["PHP_SELF"]}" method="post">
+$content
+</form>
 $warning
 EOBODY;
 
